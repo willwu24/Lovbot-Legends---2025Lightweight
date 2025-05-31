4da513c2db84d0be0c1bc74ce5f819e21f8484d8
Will Wu <willwu7370@gmail.com>
2025-05-31 15:19:10 -0700

    reverted grayscale

--- grayscale.cpp
+++ grayscale.cpp
@@ -1,12 +1,4 @@
-#include <EEPROM.h>
 #include "Grayscale.h"
-#include <PID_v1.h>
-#include <queue>
-
-// --- EEPROM Constants ---
-const int EEPROM_FLAG_ADDR = 0;
-const byte EEPROM_FLAG_VALUE = 123;
-const int EEPROM_START_ADDR = 1;
 
 // --- Pin Configuration ---
 int readPins[] = {A0, A1};
@@ -51,11 +43,11 @@ double yDistance = 0;
 // --- Corner Detection ---
 int cornerCounter = 0;
 bool inCorner = false;
-std::queue<int> pastWhites;
+std::queue<int> pastWhites;  // Unused but declared
 
 // --- Threshold Calibration ---
 int calibrationTimer = 0;
-const double sensitivity = 0.30;
+const double sensitivity = 0.30;  // Lower means more detection of white
 
 // --- PID Setup ---
 double whiteLineKp = 5.0, whiteLineKi = 0.0, whiteLineKd = 0.0;
@@ -64,15 +56,17 @@ PID whiteLinePID(&whiteLineInput, &whiteLineOutput, &whiteLineSetPoint, whiteLin
 
 // === Initialization ===
 void initSensors() {
-  for (int i = 0; i < selectorPinsSize; i++) pinMode(selectorPins[i], OUTPUT);
-  for (int i = 0; i < readPinsSize; i++) pinMode(readPins[i], INPUT);
+  for (int i = 0; i < selectorPinsSize; i++) {
+    pinMode(selectorPins[i], OUTPUT);
+  }
 
-  if (!loadThresholdsFromEEPROM()) {
-    for (int i = 0; i < sensorSize; i++) {
-      maxSensorThres[i] = 0;
-      minSensorThres[i] = 999;
-      sensorThres[i] = 0;
-    }
+  for (int i = 0; i < readPinsSize; i++) {
+    pinMode(readPins[i], INPUT);
+  }
+
+  for (int i = 0; i < sensorSize; i++) {
+    maxSensorThres[i] = 0;
+    minSensorThres[i] = 999;
   }
 
   whiteLinePID.SetMode(AUTOMATIC);
@@ -102,57 +96,12 @@ void calibrateThreshold() {
     sensorThres[i] = minSensorThres[i] + (maxSensorThres[i] - minSensorThres[i]) * sensitivity;
     sensorThres[i + 16] = minSensorThres[i + 16] + (maxSensorThres[i + 16] - minSensorThres[i + 16]) * sensitivity;
   }
-
-  saveThresholdsToEEPROM();
 }
 
 void resetCalibrateThreshold() {
   for (int i = 0; i < sensorSize; i++) {
     sensorThres[i] = 0;
   }
-}
-
-// === EEPROM Save/Load ===
-void saveThresholdsToEEPROM() {
-  int addr = EEPROM_START_ADDR;
-
-  for (int i = 0; i < sensorSize; i++) {
-    EEPROM.update(addr++, highByte(minSensorThres[i]));
-    EEPROM.update(addr++, lowByte(minSensorThres[i]));
-  }
-  for (int i = 0; i < sensorSize; i++) {
-    EEPROM.update(addr++, highByte(maxSensorThres[i]));
-    EEPROM.update(addr++, lowByte(maxSensorThres[i]));
-  }
-  for (int i = 0; i < sensorSize; i++) {
-    EEPROM.update(addr++, highByte(sensorThres[i]));
-    EEPROM.update(addr++, lowByte(sensorThres[i]));
-  }
-
-  EEPROM.update(EEPROM_FLAG_ADDR, EEPROM_FLAG_VALUE);
-  Serial.println("Thresholds saved to EEPROM.");
-}
-
-bool loadThresholdsFromEEPROM() {
-  if (EEPROM.read(EEPROM_FLAG_ADDR) != EEPROM_FLAG_VALUE) {
-    Serial.println("EEPROM not initialized, skipping load.");
-    return false;
-  }
-
-  int addr = EEPROM_START_ADDR;
-
-  for (int i = 0; i < sensorSize; i++) {
-    minSensorThres[i] = (EEPROM.read(addr++) << 8) | EEPROM.read(addr++);
-  }
-  for (int i = 0; i < sensorSize; i++) {
-    maxSensorThres[i] = (EEPROM.read(addr++) << 8) | EEPROM.read(addr++);
-  }
-  for (int i = 0; i < sensorSize; i++) {
-    sensorThres[i] = (EEPROM.read(addr++) << 8) | EEPROM.read(addr++);
-  }
-
-  Serial.println("Thresholds loaded from EEPROM.");
-  return true;
 }
 
 // === Sensor Processing ===
@@ -162,6 +111,7 @@ void processSensors() {
   whiteCountHistory = 0;
   firstWhite = false;
 
+  // Read all sensor values
   for (int i = 0; i < sensorSize / 2; i++) {
     digitalWrite(selectorPins[0], i & 0b0001);
     digitalWrite(selectorPins[1], (i & 0b0010) >> 1);
@@ -172,12 +122,18 @@ void processSensors() {
     sensorReadings[i + 16] = analogRead(readPins[1]);
   }
 
+  // Process white detection and state
   for (int i = 0; i < sensorSize; i++) {
     sensorWhite[i] = sensorReadings[i] > sensorThres[i];
     whiteCount += sensorWhite[i];
-    if (sensorWhite[i]) sensorWhiteHistory[i] = 1;
+
+    if (sensorWhite[i]) {
+      sensorWhiteHistory[i] = 1;
+    }
+
     whiteCountHistory += sensorWhiteHistory[i];
     newTouchWhite |= sensorWhite[i];
+
     if (newTouchWhite && !touchWhite) {
       touchWhite = true;
       firstWhiteIndex = sensorWhite[i] ? i : i + 32;
@@ -186,78 +142,110 @@ void processSensors() {
   }
 
   touchWhite = newTouchWhite;
+
   firstWhiteIndex = touchWhite ? firstWhiteIndex : -1;
   firstWhiteAngle = touchWhite ? ((firstWhiteIndex * (360 / 32)) - 90) % 360 : -1;
   firstWhiteAngle = (firstWhiteAngle + 360) % 360;
 }
 
-void processWhiteAngle() {
+void processWhiteAngle(){
   double xClusterSum[16] = {0};
   double yClusterSum[16] = {0};
   double clusterAngle[16] = {0};
   double sensorAngle[sensorSize] = {0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345};
+  double length = 1;
   double xSum = 0;
   double ySum = 0;
 
   int clusterCount = 0;
 
-  for (int i = 0; i < 32; i++) {
-    if (sensorWhite[i]) {
-      xClusterSum[clusterCount] += sin(toRadian(360 / sensorSize * i));
-      yClusterSum[clusterCount] += cos(toRadian(360 / sensorSize * i));
-    } else {
-      if (i > 0 && sensorWhite[i - 1]) clusterCount++;
+  for (int i = 0; i < 32; i++)
+  {
+    if (sensorWhite[i])
+    {
+      xClusterSum[clusterCount] += sin(toRadian(360/sensorSize*i));
+      yClusterSum[clusterCount] += cos(toRadian(360/sensorSize*i));
     }
-  }
-  if (sensorWhite[31]) clusterCount++;
-  if (sensorWhite[0] && sensorWhite[31]) {
-    xClusterSum[0] += xClusterSum[clusterCount - 1];
-    yClusterSum[0] += yClusterSum[clusterCount - 1];
-    xClusterSum[clusterCount - 1] = 0;
-    yClusterSum[clusterCount - 1] = 0;
-    clusterCount--;
-  }
-
-  for (int j = 0; j < 16; j++) {
-    double len = sqrt(pow(xClusterSum[j], 2) + pow(yClusterSum[j], 2));
-    if (len > 0) {
-      xClusterSum[j] /= len;
-      yClusterSum[j] /= len;
-      clusterAngle[j] = atan2(xClusterSum[j], yClusterSum[j]) * (180 / 3.14);
+    else
+    {
+      if (sensorWhite[i-1] && !sensorWhite[i])
+      {
+        clusterCount++;
+      }
     }
   }
 
-  for (int c = 0; c < clusterCount; c++) {
+  //Count last cluster
+  if (sensorWhite[31])
+  {
+    clusterCount++;
+  }
+
+  if (sensorWhite[0] && sensorWhite[31])//connect two ends?
+  {
+    xClusterSum[0] += xClusterSum[clusterCount-1];
+    yClusterSum[0] += yClusterSum[clusterCount-1];
+
+    xClusterSum[clusterCount-1] = 0;
+    yClusterSum[clusterCount-1] = 0;
+    clusterCount = clusterCount - 1;
+  }
+
+  for (int j = 0; j < 16; j++)
+  {
+    double clusterLength = sqrt(pow(xClusterSum[j],2)+pow(yClusterSum[j],2));//make it regular length
+
+    xClusterSum[j] = xClusterSum[j]/clusterLength;
+    yClusterSum[j] = yClusterSum[j]/clusterLength;
+
+    clusterAngle[j] = atan2(xClusterSum[j],yClusterSum[j]);
+    clusterAngle[j] = clusterAngle[j] * (180/3.14);
+
+  }
+
+  for (int c = 0; c < clusterCount; c++)
+  {
     xSum += xClusterSum[c];
     ySum += yClusterSum[c];
   }
 
-  double rawAngle = atan2(xSum, ySum) * 180 / 3.14;
-  rawAngle = ((int(rawAngle) + 360) % 360);
-  int finalAngle = rawAngle;
+  double rawAngle = atan2(xSum,ySum) * 180/3.14;
 
-  if (!whiteDetected()) {
-    firstFlip = true;
-  } else {
-    if (firstFlip) {
+  rawAngle = (int(rawAngle) + 360)%360;
+
+  int finalAngle = rawAngle;
+  if(!whiteDetected()){
+    firstFlip=true;
+  }
+  else{
+
+    if(firstFlip){
       firstFlip = false;
-    } else if (whiteDetected() && abs(getLightDif(lastAngle, rawAngle)) > 120) {
+    }
+    else if (whiteDetected() && abs(getLightDif(lastAngle, rawAngle)) > 120)
+    {
       flip = !flip;
       lastWhiteTime = millis();
     }
   }
-
-  if (!whiteDetected() && millis() - lastWhiteTime > 3000) {
+  if(!whiteDetected() && millis()-lastWhiteTime>3000){
     flip = 0;
   }
+  
+  whiteAngle = ((finalAngle+180*flip)+270)%360;
+  if (abs(xSum) < 0.01 && abs(ySum) < 0.01)
+  {
+    whiteAngle = previousWhiteAngle;
+  }
 
-  whiteAngle = ((finalAngle + 180 * flip) + 270) % 360;
-  if (abs(xSum) < 0.01 && abs(ySum) < 0.01) whiteAngle = previousWhiteAngle;
-
-  magnitude = sqrt(pow(xSum, 2) + pow(ySum, 2));
-  if (flip) magnitude = -magnitude;
+  magnitude = (sqrt(pow(xSum,2) + pow(ySum,2)));
+  if (flip)
+  {
+    magnitude = magnitude * -1;
+  }
 
   previousWhiteAngle = whiteAngle;
+
   lastAngle = rawAngle;
 
   whiteLineInput = abs(xSum) + abs(ySum);
@@ -266,30 +254,101 @@ void processWhiteAngle() {
   yDistance = ySum;
 }
 
-// --- Utility ---
-void resetWhiteLine() {
-  for (int i = 0; i < sensorSize; i++) {
-    sensorWhiteHistory[i] = 0;
+// --- Printing ---
+
+void printSensorsThres(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(sensorThres[sensorCount]);
+    Serial.print(", ");
   }
-  whiteCountHistory = 0;
+  Serial.println();
 }
 
-int getLightDif(int currentLight, int lastLight) {
+void printSensorsReading(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(sensorReadings[sensorCount]);
+    Serial.print(", ");
+  }
+  Serial.println();
+}
+
+void printSensorsWhite(){//print an array for white detection
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(sensorWhite[sensorCount]);
+    Serial.print(", ");
+  }
+  Serial.println();
+}
+void printWhiteSensors(){//only print the sensor index if white is detected
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    if(sensorWhite[sensorCount]){
+      Serial.print(sensorCount);
+      Serial.print(", ");
+    }
+  }
+  Serial.println();
+}
+void printMinThres(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(minSensorThres[sensorCount]);
+    Serial.print(", ");
+  
+  }
+  Serial.println();
+}
+void printMaxThres(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(maxSensorThres[sensorCount]);
+    Serial.print(", ");
+  }
+  Serial.println();
+}
+void printDiffThres(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(maxSensorThres[sensorCount]-minSensorThres[sensorCount]);
+    Serial.print(", ");
+  }
+  Serial.println();
+}
+
+void printWhiteHistory(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    Serial.print(sensorWhiteHistory[sensorCount]);
+    Serial.print(", ");
+  }
+  Serial.println();
+}
+
+// --- Utility ---
+
+void resetWhiteLine(){
+  for(int sensorCount = 0; sensorCount<sensorSize; sensorCount++){
+    sensorWhiteHistory[sensorCount]=0;
+    whiteCountHistory=0;
+  }
+}
+
+
+
+int getLightDif(int currentLight, int lastLight){//what
   int output = 0;
   int current = currentLight;
   int target = lastLight;
 
-  if (target < 180) {
-    if (current > target + 180) {
-      output = -360 + current - target;
-    } else {
-      output = current - target;
+  if(target<180){
+    if(current>target+180){
+      output = -360+current-target;
     }
-  } else {
-    if (current > target - 180) {
-      output = current - target;
-    } else {
-      output = 360 - target + current;
+    else{
+      output = current-target;
+    }
+  }
+  else{
+    if(current>target-180){
+      output = current-target;
+    }
+    else{
+      output = 360-target+current;
     }
   }
   return output;
